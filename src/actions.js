77241f7acafcdbd64fc461e3c5a9401633989683
Manjit Raj <96285005+manjitrajgithub@users.noreply.github.com>
2023-07-28 17:45:17 +0530

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -13,3 +13,35 @@ export const repairAdded = (var1,va2) => {
 }
 
 */
+let nextId = 1;
+
+export const repairAdded = (owner, model, description) => ({
+  type: 'repairAdded',
+  payload: {
+    id: nextId++,
+    owner,
+    model,
+    description,
+    resolved: false,
+  },
+});
+
+export const repairRemoved = (id) => ({
+  type: 'repairRemoved',
+  payload: { id },
+});
+
+export const repairResolved = (id) => ({
+  type: 'repairResolved',
+  payload: { id },
+});
+
+export const repairUpdated = (id, owner, model, description) => ({
+  type: 'repairUpdated',
+  payload: { id, owner, model, description },
+});
+
+export const editTask = (id, owner, model, description) => ({
+  type: 'editTask',
+  payload: { id, owner, model, description },
+});
