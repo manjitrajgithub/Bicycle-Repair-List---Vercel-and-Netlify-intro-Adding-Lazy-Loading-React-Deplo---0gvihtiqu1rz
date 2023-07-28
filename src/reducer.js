0663e9a1e71cbd44887f8e0f8db27feff7af8f2d
Manjit Raj <96285005+manjitrajgithub@users.noreply.github.com>
2023-07-28 17:47:12 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,21 +1,78 @@
-/*
-reducer is defined here and is exproted.
-This reducer should handle all the actions.
-Example of how to use reducer is as follows:
+const initialState = {
+  items: [],
+  item: {
+    owner: '',
+    model: '',
+    description: '',
+  },
+  editMode: false,
+};
 
-const reducer = (state = [],action = {}) => {
+const bicycleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'repairAdded':
+      if (!action.payload.owner || !action.payload.model || !action.payload.description) {
+        return state;
+      }
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+        item: {
+          owner: '',
+          model: '',
+          description: '',
+        },
+      };
 
-  switch(action.type){
+    case 'repairRemoved':
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload.id),
+      };
 
-    case 'actionType1': 
-      return changedState1;
-    
-    case 'actionType2':
-      return changedState2;
+    case 'repairResolved':
+      return {
+        ...state,
+        items: state.items.map((item) =>
+          item.id === action.payload.id ? { ...item, resolved: !item.resolved } : item
+        ),
+      };
 
-    default: 
+    case 'repairUpdated':
+      return {
+        ...state,
+        items: state.items.map((item) =>
+          item.id === action.payload.id
+            ? {
+                ...item,
+                owner: action.payload.owner,
+                model: action.payload.model,
+                description: action.payload.description,
+              }
+            : item
+        ),
+        item: {
+          owner: '',
+          model: '',
+          description: '',
+        },
+        editMode: false,
+      };
+
+    case 'editTask':
+      return {
+        ...state,
+        item: {
+          owner: action.payload.owner,
+          model: action.payload.model,
+          description: action.payload.description,
+        },
+        editMode: true,
+      };
+
+    default:
       return state;
-    }
-  
-export default reducer;
-*/
+  }
+};
+
+export default bicycleReducer;
