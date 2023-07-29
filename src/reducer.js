bd4e455b1fce5cebaa227d5789cfc7da126d5d88
Manjit Raj <96285005+manjitrajgithub@users.noreply.github.com>
2023-07-29 20:25:07 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -14,18 +14,57 @@ const bicycleReducer = (state = initialBicycleState, action) => {
     case 'repairAdded':
       // Add a new repair to the list
       // Return updated state
+      ...state,
+        items: [...state.items, action.payload],
+        item: {
+          owner: '',
+          model: '',
+          description: ''
+        }
+      };
     case 'repairRemoved':
       // Remove a repair from the list
       // Return updated state
+  return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload.id)
+      };
     case 'repairResolved':
       // Mark a repair as resolved or unresolved
       // Return updated state
+  return {
+        ...state,
+        items: state.items.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, resolved: !item.resolved }
+            : item
+        )
+      };
     case 'repairUpdated':
       // Update details of a repair
       // Return updated state
+  return {
+        ...state,
+        items: state.items.map((item) =>
+          item.id === action.payload.id ? action.payload : item
+        ),
+        editMode: false,
+        item: {
+          owner: '',
+          model: '',
+          description: ''
+        }
+      };
     case 'editTask':
       // Set the edit mode and item for a repair being updated
       // Return updated state
+  return {
+        ...state,
+        editMode: true,
+        item: {
+          ...action.payload
+        }
+      };
     default:
       return state;
   }
